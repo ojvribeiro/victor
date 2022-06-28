6f1a945f4e2ea1592a497ae9d6f0bb34fe3cb623
Victor Ribeiro <devjvribeiro@gmail.com>
2022-06-28 08:42:53 -0300

    Criada órbita de tecnologias

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,39 @@
 <template>
   <div>
-    <header class="h-screen bg-gradient-to-r from-[#3c06a7] to-[#237d98]">
+    <header class="h-screen bg-gradient-to-r from-[#3c06a7] to-[#237d98] overflow-hidden">
       <div class="grid grid-cols-12 gap-8 h-full pt-10 lg:pt-0">
         <div class="col-span-12 lg:col-span-3 lg:col-start-3 flex items-center justify-center">
-          <div class="ping rounded-full overflow-hidden aspect-square w-[100px] md:w-[200px] xl:w-[300px] ring-4">
-            <img src="~/assets/img/avatar/me.jpg" alt="" class="w-full object-cover">
+          <div class="relative">
+            <div class="orbit">
+              <div class="orbit-object">
+                <i class="fab fa-sass"></i>
+              </div>
+
+              <div class="orbit-object">
+                <i class="fab fa-js"></i>
+              </div>
+
+              <div class="orbit-object">
+                <i class="fab fa-vuejs"></i>
+              </div>
+
+              <div class="orbit-object">
+                <i class="fab fa-react"></i>
+              </div>
+
+              <div class="orbit-object">
+                <i class="fab fa-bootstrap"></i>
+              </div>
+
+              <div class="orbit-object">
+                <i class="fab fa-wordpress"></i>
+              </div>
+            </div>
+
+            <div class="ping relative rounded-full overflow-hidden aspect-square w-[100px] md:w-[200px] xl:w-[300px] ring-4">
+              
+              <img src="~/assets/img/avatar/me.jpg" alt="" class="w-full object-cover">
+            </div>
           </div>
         </div>
 
@@ -56,5 +85,95 @@
 
 
 <style scoped>
-  @import 'https://pro.fontawesome.com/releases/v5.10.0/css/all.css';
+  @import 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/brands.min.css';
+</style>
+
+
+<style lang="scss" scoped>
+  $time: 40s;
+
+  i.fab {
+    font-style: normal;
+  }
+
+  .orbit {
+    height: 160%;
+    left: -30%;
+    position: absolute;
+    top: -30%;
+    transform: translate(-50%,-50%);
+    transform-origin: center center;
+    width: 160%;
+    border-radius: 50%;
+    animation: orbit-animation linear $time infinite;
+
+    .orbit-object {
+      position: absolute;
+      border-radius: 50%;
+      width: 60px;
+      height: 60px;
+      aspect-ratio: 1;
+      background-color: rgba(#fff, 7%);
+      animation: orbit-compensation linear $time infinite;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      i.fab {
+        font-style: normal;
+        font-size: 2rem;
+        color: rgba(#fff, 20%);
+      }
+      
+      &:nth-child(1) {
+        top: 43px;
+        left: 14px;
+      }
+
+      &:nth-child(2) {
+        top: -55px;
+        left: 171px;
+      }
+
+      &:nth-child(3) {
+        top: 392px;
+        left: 330px;
+      }
+
+      &:nth-child(4) {
+        top: 1px;
+        left: 347px;
+      }
+
+      &:nth-child(5) {
+        top: 364px;
+        left: 1px;
+      }
+
+      &:nth-child(6) {
+        left: 438px;
+        top: 234px;
+      }
+
+      @keyframes orbit-animation {
+        0% {
+          transform: rotate(0deg);
+        }
+
+        100% {
+          transform: rotate(360deg);
+        }
+      }
+
+      @keyframes orbit-compensation {
+        0% {
+          transform: rotate(0deg);
+        }
+
+        100% {
+          transform: rotate(-360deg);
+        }
+      }
+    }
+  }
 </style>
